958d0b7954e7146ad812168297012eb3c4254a83
Paaksing <paaksingtech@gmail.com>
2022-02-08 17:37:26 -0400

    New: Add info for shell reloading

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 
 default:
 	@pip3 install dumbtils -U
+	@echo "INFO: Reload shell or virtual environment for new commands to take effect"
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="dumbtils",
-    version="0.2.0",
+    version="0.2.1",
     author="Paaksing",
     author_email="paaksingtech@gmail.com",
     url="https://github.com/paaksing/dumbtils",
